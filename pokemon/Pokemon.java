f18c4a21a1f58a2c8a51462a7cf5f1477666c012
ertaekkarat <ekkarut_1111@hotmail.com>
2017-05-09 23:48:39 +0700

    ErtPogemon

---
+++ pokemon/Pokemon.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokemon;
+
+/**
+ *
+ * @author Rock
+ */
+public class Pokemon {
+    String Name;
+    int HP;
+    int Atk;
+    int Def;
+    String Skill_1;
+    int Dm_1;
+    String Skill_2;
+    int Dm_2;
+    String Skill_3;
+    int Dm_3;
+    String Skill_4;
+    int Dm_4;
+    
+        
+    
+}
+class Pikachu extends Pokemon{
+
+    public void Pikachu()
+    {         
+        this.Name = "Pikachu";
+        this.HP = 20;
+        this.Atk = 30;
+        this.Def = 20;
+        this.Skill_1 = "Static";
+        this.Dm_1 = 23;
+        this.Skill_2 = "Electro Ball";
+        this.Dm_2 = 30;
+        this.Skill_3 = "Iron Tail";
+        this.Dm_3 = 30;
+        this.Skill_4 = "Thunder Shock";
+        this.Dm_4 = 10;
+    }
+    
+}
+class Bulbasaur extends Pokemon{
+     public void Bulbasaur()
+    {         
+        this.Name = "Bulbasaur";
+        this.HP = 20;
+        this.Atk = 30;
+        this.Def = 20;
+        this.Skill_1 = "Overgrow";
+        this.Dm_1 = 20;
+        this.Skill_2 = "Razor Leaf";
+        this.Dm_2 = 20;
+        this.Skill_3 = "Bullet Seed";
+        this.Dm_3 = 10;
+        this.Skill_4 = "Poison Powder";
+        this.Dm_4 = 60;
+    }
+}
+class Exeggutor extends Pokemon{
+     public void Exeggutor()
+    {         
+        this.Name = "Exeggutor";
+        this.HP = 40;
+        this.Atk = 50;
+        this.Def = 40;
+        this.Skill_1 = "Chlorophyll";
+        this.Dm_1 = 30;
+        this.Skill_2 = "Shake it off";
+        this.Dm_2 = 20;
+        this.Skill_3 = "Seed Bomb";
+        this.Dm_3 = 80;
+        this.Skill_4 = "String Bomb";
+        this.Dm_4 = 30;
+    }
+}
+class Geodude extends Pokemon{
+     public void Geodude()
+    {         
+        this.Name = "Geodude";
+        this.HP = 20;
+        this.Atk = 40;
+        this.Def = 40;
+        this.Skill_1 = "Rock Head";
+        this.Dm_1 = 10;
+        this.Skill_2 = "Sturdy";
+        this.Dm_2 = 20;
+        this.Skill_3 = "Knuckle Punch";
+        this.Dm_3 = 10;
+        this.Skill_4 = "Focus Fist";
+        this.Dm_4 = 30;
+    }
+}
+class Ponyta extends Pokemon{
+     public void Ponyta()
+    {         
+        this.Name = "Ponyta";
+        this.HP = 20;
+        this.Atk = 40;
+        this.Def = 30;
+        this.Skill_1 = "Run away";
+        this.Dm_1 = 20;
+        this.Skill_2 = "Flash Fire";
+        this.Dm_2 = 20;
+        this.Skill_3 = "Smash kick";
+        this.Dm_3 = 10;
+        this.Skill_4 = "Flame Tail";
+        this.Dm_4 = 20;
+    }
+}
+class Seadra extends Pokemon{
+     public void Seadra()
+    {         
+        this.Name = "Seadra";
+        this.HP = 20;
+        this.Atk = 30;
+        this.Def = 40;
+        this.Skill_1 = "Poison Point";
+        this.Dm_1 = 15;
+        this.Skill_2 = "Sniper";
+        this.Dm_2 = 30;
+        this.Skill_3 = "Aqua Trick";
+        this.Dm_3 = 30;
+        this.Skill_4 = "Water Gun";
+        this.Dm_4 = 30;
+    }
+}
+
+
+
---
+++ pokemon/PokemonInheritance.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokemon;
+
+/**
+ *
+ * @author Rock
+ */
+public class PokemonInheritance {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJFrame frame = new NewJFrame();
+        frame.setVisible(true);
+           
+    }
+    
+}
